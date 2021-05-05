4f53f1562e970f70c60f7d845ecab70be733fb30
arendach <arendach.taras@gmail.com>
2021-05-05 13:54:26 +0300

    change field from wysywig to textarea

--- src/config/gallery.php
+++ src/config/gallery.php
@@ -14,7 +14,7 @@ return array(
         ),
         'description' => array(
             'caption' => 'Описание',
-            'type' => 'wysiwyg',
+            'type' => 'textarea',
             'field' => 'text',
             'tabs' => config('translations.config.languages')
         ),
--- src/resources/views/partials/fields_types/field_textarea.blade.php
+++ src/resources/views/partials/fields_types/field_textarea.blade.php
@@ -1,5 +1,5 @@
 <div class="imgInfoBox-relative-block">
     <label class="input">
-        <textarea rows="{{$field['rows'] or '3'}}"  id="{{$fieldName}}"  name="{{ $fieldName }}" class="custom-scroll imgInfoBox-textarea">{{$entity->$fieldName}}</textarea>
+        <textarea rows="{{isset($field['rows']) ? $field['rows'] : '3'}}"  id="{{$fieldName}}"  name="{{ $fieldName }}" class="custom-scroll imgInfoBox-textarea">{{$entity->$fieldName}}</textarea>
     </label>
 </div>
